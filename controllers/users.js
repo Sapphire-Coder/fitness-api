ca89c9a225730f521269a92c4d951d7168dc17c5
Sapphire-Coder <74931786+Sapphire-Coder@users.noreply.github.com>
2022-04-25 11:26:06 -0400

    Added user model and routes

---
+++ controllers/users.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/users')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+// Index (testing purposes only)
+router.get('/', (req, res) => {
+    User.find({}, (err, foundUsers) => {
+        res.json(foundUsers)
+    })
+})
+
+// Create
+router.post('/', async (req, res) => {   
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        let user = { username: req.body.username, password: hashedPassword, name: req.body.name}
+        User.create(user, (err, createdUser) => {
+            res.json(createdUser)
+        })
+    }
+    catch {
+        res.status(500).send()
+    }
+})
+
+// Login
+router.post('/login', async (req, res) => {
+    const user = await User.findOne({ username: req.body.username })
+
+    if(!user) {
+        return res.status(400).send('Cannot find user')
+    }
+
+    try{
+        if(await bcrypt.compare(req.body.password, user.password)){
+            const id = { user: username._id }
+            const accessToken = jwt.sign(id, process.env.ACCESS_TOKEN_SECRET)
+            return res.json({ accessToken: accessToken })
+        }
+        else{
+            return res.status(500).send()
+        }
+    }
+    catch{
+        return res.status(500).send()
+    }
+})
+
+module.exports = router
---
+++ models/workouts.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const workoutSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    reps: { type: Number, required: true },
+    sets: { type: Number, required: true},
+    calories: { type: Number, required: true},
+    user: { required: true }
+})
+
+const Workout = mongoose.model('Workout', workoutSchema)
+
+module.exports = Workout
